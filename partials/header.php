edc264269feaffbf0cbf5478d9943007e2f60f96
Adam Pflantzer <apflantzer@gmail.com>
2015-01-14 15:48:58 -0500

    Remove Optimizely until prod version

--- partials/header.php
+++ partials/header.php
@@ -13,6 +13,5 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/all.css">
     <title>GS</title>
-    <script src="//cdn.optimizely.com/js/317810835.js"></script>
     <script src="js/modernizr-2.6.2.min.js"></script>
 </head>
